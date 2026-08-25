cf6a089d18dd3c584cbced873a8d470a9aee6843
Naboday Mete <nabodaymete02@gmail.com>
2026-08-25 09:12:37 +0530

    Add localStorage-backed data layer for ledgers, sections, and blocks

---
+++ src/data/index.js
@@ -0,0 +1,20 @@
+export {
+  getLedgers,
+  getLedger,
+  createLedger,
+  updateLedger,
+  deleteLedger,
+  getLedgerProgress,
+} from './ledgers.js'
+
+export {
+  createSection,
+  updateSection,
+  deleteSection,
+  reorderSections,
+  toggleSectionReviewed,
+} from './sections.js'
+
+export { addBlock, updateBlock, deleteBlock, reorderBlocks } from './blocks.js'
+
+export { generateId } from './id.js'
